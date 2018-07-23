055e92c1c9ab89f6f8c69eaf62135fc3ba14a23b
Maciej <Maciej@DESKTOP-MUQ3U08>
2018-07-23 15:06:16 +0200

    added form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  languages = ["English", "Spanish", "Polish"];
+  model = new Employee('Jaś', 'Fasola', false, "w2", "Polish", null);
 }
